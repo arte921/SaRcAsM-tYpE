4b12604d05142c8b2e4a270480aa9f8d3888585d
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-15 09:36:03 +0100

    l

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var input,output,i,length;
 var mode = 'ff';
+var ff = true;
 function update(){
   output='';
   input=document.getElementById('input').value;
   length=input.length;
 
-  switch(mode){
-    case 'ff':
+  if(ff){
     for(i=0;i<length;i++){
       if(i%2==0){
         output += input.charAt(i).toLowerCase();
@@ -14,7 +14,14 @@ function update(){
         output += input.charAt(i).toUpperCase();
       }
     }
-    break;
+  }else{
+    for(i=0;i<length;i++){
+      if(Math.random()>document.getElementById('randomslider').value / 100){
+        output += input.charAt(i).toLowerCase();
+      }else{
+        output += input.charAt(i).toUpperCase();
+      }
+    }
   }
   document.getElementById('output').innerHTML = output;
 }
